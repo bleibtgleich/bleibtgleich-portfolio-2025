1eb29a0b9f1ca30995bccb9b22503dacad4f0a30
bleibtgleich <kellerman6969@gmail.com>
2025-02-19 15:59:42 +0200

    Update smooth-scroll.js

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,32 +1,32 @@
 if (typeof gsap !== 'undefined' && typeof ScrollTrigger !== 'undefined' && typeof window.Lenis !== 'undefined') {
-    gsap.registerPlugin(ScrollTrigger);
-  
-    let lenis;
-  
-    const initSmoothScrolling = () => {
-      lenis = new window.Lenis({
-        lerp: 0.1,
-        smoothWheel: true,
-        direction: 'vertical',
-        gestureDirection: 'vertical',
-        smoothTouch: false,
-        touchMultiplier: 2,
-        infinite: false,
-      });
-  
-      lenis.on('scroll', ScrollTrigger.update);
-  
-      function raf(time) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-      }
-  
+  gsap.registerPlugin(ScrollTrigger);
+
+  let lenis;
+
+  const initSmoothScrolling = () => {
+    lenis = new window.Lenis({
+      lerp: 0.1,
+      smoothWheel: true,  // Для десктопного скроллинга колесом мыши
+      direction: 'vertical',
+      gestureDirection: 'vertical',
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    function raf(time) {
+      lenis.raf(time);
       requestAnimationFrame(raf);
-  
-      console.log('Lenis initialized:', lenis);
-    };
-  
-    window.addEventListener('load', initSmoothScrolling);
-  } else {
-    console.error('One or more libraries (GSAP, ScrollTrigger, or Lenis) are not loaded.');
-  }
+    }
+
+    requestAnimationFrame(raf);
+
+    console.log('Lenis initialized:', lenis);
+  };
+
+  window.addEventListener('load', initSmoothScrolling);
+} else {
+  console.error('One or more libraries (GSAP, ScrollTrigger, or Lenis) are not loaded.');
+}
